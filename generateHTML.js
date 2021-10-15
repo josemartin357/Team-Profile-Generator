de6d47a84ae043793e711d02ebeb2c4cb0882cc1
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-10-15 00:16:15 -0500

    Added html and functions to pull data for manager, intern and engineer

--- generateHTML.js
+++ generateHTML.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // classes for these roles
 const Manager = require('./manager');
@@ -70,35 +69,3 @@ function pullInternInfo(){
 
 // exporting module to generateHTML
 module.exports = generateHTML;
-=======
-
-// classes for these roles
-const Manager = require('./manager');
-const Engineer = require('./engineer');
-const Intern = require('./intern');
-
-// function to make div with required info
-function makeCard() {
-    // switch statement to jump between roles
-    switch(teamList.getRole()) {
-      case 'Manager':
-        const manager = new Manager(teamList.name, teamList.id, teamList.email, teamList.officeNumber);
-        // pushing info to pullManagerInfo
-        memberInfo.push(pullManagerInfo(manager));
-        break;
-      case 'Engineer':
-        const engineer = new Engineer(teamList.name, teamList.id, teamList.email, teamList.github);
-        // pushing info to pullEngineerInfo
-        memberInfo.push(pullEngineerInfo(engineer));
-        break;
-      case 'Intern':
-        const intern = new Intern(teamList.name, teamList.id, teamList.email, teamList.school);
-        // pushing info to pullInternInfo
-        memberInfo.push(pullInternInfo(intern));
-        break;
-    }
-  }
-
-// exporting module to generateHTML
-module.exports = generateHTML;
->>>>>>> 9b5ca096af3f47f95e1e90ea3c215536ed6ba22a
